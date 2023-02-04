47bc0c9bf8ea33ad1320a776b5f94efb18380695
Olga <olgabutyrina2020@gmail.com>
2023-02-05 00:38:43 +0800

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Напишите, пожалуйста, пятизначное число: ");
+string[] array = { Console.ReadLine() };
+
+int n = array.Length;
+int i = 0;
+
+if (n == 5)
+{
+    while (i<=n)
+    {
+        if (array[0]==array[4] || array[1]==array[3])
+        {
+            System.Console.WriteLine("Введенное число является палиндромом");
+        }
+        else
+        {
+            System.Console.WriteLine("Введенное число не является палиндромом");
+        }
+    i++;
+    }
+}
+else
+{
+    System.Console.WriteLine("Введено не пятизначное число");
+}
