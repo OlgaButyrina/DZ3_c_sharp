7588c484ed85b42138e7c3198e93ca71070c4e29
Olga <olgabutyrina2020@gmail.com>
2023-02-05 21:04:25 +0800

    changed task23

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите, пожалуйста, число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < N; i++)
+{
+    System.Console.Write(Math.Pow(i, 3) + ", ");
+}
+System.Console.Write(Math.Pow(N, 3));
